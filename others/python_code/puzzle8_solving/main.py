1f74fcfcc0444a25bb7107210f5091cf4e5a650b
memeda <readonlyfile@hotmail.com>
2015-12-29 11:09:08 +0800

    add some for a star search

--- others/python_code/puzzle8_solving/main.py
+++ others/python_code/puzzle8_solving/main.py
@@ -1,22 +1,51 @@
 #coding=utf8
 
+import Queue
+import logging
 from state import Puzzle8State
 
+logging.basicConfig(level=logging.INFO)
+
 def a_star_search(init_state , target_state) :
-    
-
-
+    heap = Queue.PriorityQueue()
+    heap.put(init_state)
+    print '++++++++++++++'
+    cnt = 0
+    while True :
+        if heap.empty() :
+            logging.info("Heap has been Empty . Stop")
+            break
+        cnt += 1
+        cur_state = heap.get()
+        if cur_state.is_same2other_state(target_state) :
+            logging.info("Get solving .")
+            break
+        states_lst = cur_state.expand_states()
+        for state in states_lst : 
+            state.set_cost4a_star(target_state)
+            heap.put(state)
+            #print state
+            #print state._predict_score2target_state(target_state)
+        if cnt%100 == 0 :
+            print heap.qsize()
+            print cur_state
 def main() :
-    init_puzzle_data_1d = [2,3,4,1,5,6,7,8]
+    init_puzzle_data_1d = [2,3,5,1,4,6,8,7]
     init_blank_row = 1
-    init_blank_col = 1
+    init_blank_col = 3
     init_state = Puzzle8State()
     init_state.init_puzzle_data(init_puzzle_data_1d , init_blank_row , init_blank_col)
 
-    target_puzzle_data_1d = [1,2,3,4,5,6,7,8]
+    target_puzzle_data_1d = [1,2,3,8,4,7,6,5]
     target_blank_row = 2
     target_blank_col = 2
     target_state = Puzzle8State()
     target_state.init_puzzle_data(target_puzzle_data_1d , target_blank_row , target_blank_col)
-
+    
+    print init_state
+    print target_state
+    print init_state._predict_score2target_state(target_state)
     a_star_search(init_state , target_state)
+
+if __name__ == "__main__" :
+    main() 
--- others/python_code/puzzle8_solving/state.py
+++ others/python_code/puzzle8_solving/state.py
@@ -10,8 +10,8 @@ class Puzzle8State(object) :
     EXPAND_RIGHT = 3
     def __init__(self) :
         self.puzzle_data = None
-        self.blank_row = -1
-        self.blank_col = -1
+        self.blank_row = 0 # it count from 1 , not 0 ! because it is an interface to outer .
+        self.blank_col = 0
         self.history_states = []
         self.having_cost = 0 # the cost from root to current , g(x)
         self.total_cost = 0 # g(x) + h*(x)
@@ -25,16 +25,20 @@ class Puzzle8State(object) :
         self.blank_row = blank_row
         self.blank_col = blank_col
 
-    def _parsing_1d_puzzle_data(puzzle_data_1d , blank_row , blank_col) :
+    def _parsing_1d_puzzle_data(self , puzzle_data_1d , blank_row , blank_col) :
         '''
         1-d puzzle data to 2-d puzzle data , make it more clearly 
         Note : the return data is independent with the input puzzle_data in the memory .(slice operation has copy the data)
         '''
         assert(len(puzzle_data_1d) == 8)
         row_idx = ( blank_row - 1 ) * 3 + blank_col - 1 
-        complete_puzzle = copy.copy(puzzle_data_1d).insert(row_idx , config.BLANK)
+        complete_puzzle = copy.copy(puzzle_data_1d)
+        complete_puzzle.insert(row_idx , config.BLANK)
         return [ complete_puzzle[row * 3 : (row + 1)* 3] for row in range(3) ]
     
+    def is_same2other_state(self , target) :
+        return self._predict_score2target_state(target) == 0
+
     def _predict_score2target_state(self , target) :
         '''
         h*(x)
@@ -48,54 +52,67 @@ class Puzzle8State(object) :
         '''
         f(x) = g(x) + h*(x)
         '''
-        self.total_cost = self.having_cost + self._predict_score2target_state
+        #self.total_cost = self.having_cost + self._predict_score2target_state(target)
+        self.total_cost = self._predict_score2target_state(target)
     
     def add_previous_state2history(self , pre_state) :
         self.history_states.append(pre_state)
 
     def _build_expand_state(self , expand_type) :
-       '''
-       According to the expand type to build a puzzle state
-       '''
-        state = Puzzle2State()
+        '''
+        According to the expand type to build a puzzle state
+        '''
+        state = Puzzle8State()
         state.having_cost = self.having_cost + 1 # update root to new state cost 
         state.puzzle_data = copy.deepcopy(self.puzzle_data) 
-        if expand_type = Puzzle2State.EXPAND_UP :
+        if expand_type == Puzzle8State.EXPAND_UP :
             state.blank_row = self.blank_row - 1
             state.blank_col = self.blank_col 
-        elif expand_type == Puzzle2State.EXPAND_DOWN :
+        elif expand_type == Puzzle8State.EXPAND_DOWN :
             state.blank_row = self.blank_row + 1
             state.blank_col = self.blank_col 
         elif expand_type == Puzzle8State.EXPAND_LEFT :
             state.blank_row = self.blank_row
-            state.blank_col = self.blank_col -1
+            state.blank_col = self.blank_col - 1
         elif expand_type == Puzzle8State.EXPAND_RIGHT :
             state.blank_row = self.blank_row
             state.blank_col = self.blank_col + 1
+        else :
+            logging.error("invlaid expand type")
         # swap original blank element and new blank element
-        state.puzzle_data[state.blank_row][state.blank_col] , state.puzzle_data[self.blank_row][self.blank_col] = (
-                state.puzzle_data[self.blank_row][self.blank_col] , state.puzzle_data[state.blank_row][state.blank_col] )
+        state.puzzle_data[state.blank_row-1][state.blank_col-1] , state.puzzle_data[self.blank_row-1][self.blank_col-1] = (
+                state.puzzle_data[self.blank_row-1][self.blank_col-1] , state.puzzle_data[state.blank_row-1][state.blank_col-1] )
+        state.history_states = copy.copy(self.history_states)
         state.add_previous_state2history(self)
         return state
 
     def expand_states(self) :
         states_lst = []
+        ## clearly ! blank_row , blank_col count from 1 ! 
         # to up
-        if self.blank_row > 0  :
+        if self.blank_row > 1  :
             states_lst.append(self._build_expand_state(Puzzle8State.EXPAND_UP))
         # to down
-        if self.blank_row < 2 :
+        if self.blank_row < 3 :
             states_lst.append(self._build_expand_state(Puzzle8State.EXPAND_DOWN))
         # to left
-        if self.blank_col > 0 :
+        if self.blank_col > 1 :
             states_lst.append(self._build_expand_state(Puzzle8State.EXPAND_LEFT))
         # to right
-        if self.blank_col < 2 :
+        if self.blank_col < 3 :
             states_lst.append(self._build_expand_state(Puzzle8State.EXPAND_RIGHT))
         return states_lst
 
-    def print_state(self) :
+    def __str__(self) :
         node_str_lst = [ str(node) if node != config.BLANK else " " 
                          for row in self.puzzle_data for node in row ]
         lines = [ "{0:^3}{1:^3}{2:^3}".format(*node_str_lst[3*row_id : 3*(row_id+1) ]) for row_id in range(3) ]
+        cost_info = "g(n) = {0} , f(n) = {1}".format(self.having_cost , self.total_cost)
+        return "\n".join(lines + [ cost_info , ] )
 
+    def __cmp__(self , other_state) :
+        '''
+        According to the total state ( that is , f(n) ) to rank the state . 
+        Smaller is in front 
+        '''
+        return cmp(self.total_cost , other_state.total_cost)
