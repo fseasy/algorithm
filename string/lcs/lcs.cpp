46a077e3a5a8257d6e6ee0494c819b6cde61e411
memeda <readOnlyFile@hotmail.com>
2017-11-12 00:24:20 +0800

    fix naive spelling bug && impl dp lcs

--- string/lcs/lcs.cpp
+++ string/lcs/lcs.cpp
@@ -1,5 +1,7 @@
 #include "lcs.h"
 
+#include <algorithm>
+
 namespace algorithm_impl {
 namespace string {
 
@@ -11,7 +13,7 @@ int LCS::naive_lcs_impl(const std::string::const_iterator &s1_beg,
     if (s1_beg == s1_end || s2_beg == s2_end) {
         return 0;
     }
-    auto s1_last_char_iter = s2_end - 1;
+    auto s1_last_char_iter = s1_end - 1;
     auto s2_last_char_iter = s2_end - 1;
     if (*s1_last_char_iter == *s2_last_char_iter) {
         return 1 + naive_lcs_impl(s1_beg, s1_last_char_iter, s2_beg,
@@ -23,7 +25,37 @@ int LCS::naive_lcs_impl(const std::string::const_iterator &s1_beg,
     }
 }
 
-int LCS::lcs(const std::string &s1, const std::string &s2) {}
+int LCS::lcs(const std::string &s1, const std::string &s2) {
+    using std::vector;
+    auto s1_len = s1.size();
+    auto s2_len = s2.size();
+    if (s1_len == 0 || s2_len == 0){
+        return 0;
+    }
+    // based on 0 -> lcs[m, n] = table[m-1][n-1]
+    vector<vector<int>> dp_table(s1_len, vector<int>(s2_len, 0));
+    // set edge
+    dp_table[0][0] = s1[0] == s2[0] ? 1 : 0;
+    for (auto i = 1UL; i < s1_len; ++i) {
+        dp_table[i][0] = s1[i] == s2[0] ? 1 : dp_table[i - 1][0] ;
+    }
+    for (auto i = 1UL; i < s2_len; ++i) {
+        dp_table[0][i] = s1[0] == s2[i] ? 1 : dp_table[0][i - 1];
+    }
+    // set others slots
+    for (auto row_id = 1UL; row_id < s1_len; ++row_id) {
+        for (auto col_id = 1U; col_id < s2_len; ++col_id) {
+            if(s1[row_id] == s2[col_id]) {
+                dp_table[row_id][col_id] = 1 + dp_table[row_id - 1][col_id - 1];
+            } else {
+                dp_table[row_id][col_id] = std::max(dp_table[row_id][col_id - 1],
+                    dp_table[row_id - 1][col_id]);
+            }
+
+        }
+    }
+    return dp_table[s1_len - 1][s2_len - 1];
+}
 
 } // end of namespace string
 } // end of namespace algorithm_impl
--- string/lcs/test_lcs.cpp
+++ string/lcs/test_lcs.cpp
@@ -1,15 +1,47 @@
 #include <iostream>
+#include <tuple>
+#include <cassert>
 
 #include "lcs.h"
 
-int main() {
+void test(){
+    using std::vector;
+    using std::string;
+    using std::tuple; // pair is enough, just test.
+    using std::make_tuple;
     using namespace algorithm_impl::string;
+
+    vector<tuple<string, string>> test_pairs = {
+        make_tuple("", ""),
+        make_tuple("", "a"),
+        make_tuple("abc", "ac"),
+        make_tuple("bcdae", "km"),
+        tuple<string, string>{"bcdae", "ca"},
+        tuple<string, string>{"bacbkmf", "bckm"}
+    };
+    for(const auto& str_pair : test_pairs) {
+        const string& s1 = std::get<0>(str_pair);
+        const string& s2 = std::get<1>(str_pair);
+        int naive_result = LCS::naive_lcs(s1, s2);
+        int dp_result = LCS::lcs(s1, s2);
+        if (naive_result != dp_result) {
+            std::cout << s1 << "\n" << s2 << "\n"
+                      << "naive lcs = " << naive_result << "\n"
+                      << "dp lcs = " << dp_result << "\n";
+            exit(1);
+        }
+    }
+
+}
+
+int main() {
+
     using namespace std;
 
     string s1 = "abcd";
     string s2 = "bd";
+    test();
+    cerr << "all pass" << endl;
 
-    cout << LCS::naive_lcs(s1, s2) << endl;
-    
     return 0;
 }
